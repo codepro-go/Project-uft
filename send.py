83465ddac700599e553915923f2a608aa8c0e2a4
codepro-go <73317365+codepro-go@users.noreply.github.com>
2021-03-20 17:42:59 -0700

    Add files via upload

---
+++ send.py
@@ -0,0 +1,12 @@
+######part2p1
+try:
+    import read
+    SEND=read.p2
+except:
+    print("error: part2p1:get")
+######part2p2
+try:
+    SEND1 = "".join(SEND)
+    print(SEND1)
+except:
+    print("error: part2p2:get")
