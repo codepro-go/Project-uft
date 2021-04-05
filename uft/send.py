f3a7ae24caa325ac2e0b0b65e1b44399f4e0f562
codepro-go <73317365+codepro-go@users.noreply.github.com>
2021-04-05 12:13:22 -0700

    Update send.py
    There is a little edit and getting rid of trash code

--- uft/send.py
+++ uft/send.py
@@ -6,7 +6,8 @@ except:
     print("error: part2p1:get")
 ######part2p2
 try:
-    SEND1 = "".join(SEND)
-    print(SEND1)
-except:
+    first_of_send = SEND[0]
+    second_of_send = SEND[1]
+    # in futer Got_from_application = Tkapp
+execpt:
     print("error: part2p2:get")
